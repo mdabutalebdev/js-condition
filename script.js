77c69dac8a2d579009927b5e42f822065fa1b3f7
mdabutalebdev <mdabutaleb.dev@gmail.com>
2025-08-13 22:54:47 +0600

    1st condition done

---
+++ script.js
@@ -0,0 +1,24 @@
+// একটি ভেরিয়েবল age আছে।
+// যদি বয়স 18 বা তার বেশি হয়, আউটপুট দেখাবে "আপনি ভোট দিতে পারবেন"
+// না হলে আউটপুট দেখাবে "আপনি ভোট দিতে পারবেন না"
+
+let boys = 10;
+const votDewarBoys = 18;
+
+if (boys >= votDewarBoys) {
+  console.log("apni vot dite parben");
+} else {
+  console.log("apni akhono soto age boro hon 18 hoile vot diyen");
+}
+
+// পাস বা ফেল
+// একটি ভেরিয়েবল marks আছে।
+// যদি নম্বর 40 বা তার বেশি হয়, আউটপুট "আপনি পাস করেছেন"
+// না হলে "আপনি ফেল করেছেন"
+
+let marks = 60;
+const passMark = 40;
+
+if (marks >= passMark) {
+  console.log("Obinondhon apni pass koresen. Misty niye aisen");
+}
