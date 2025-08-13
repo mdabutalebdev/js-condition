2bd6632b3bd28e79dc1a6b14c814a85e9e5b1a77
mdabutalebdev <mdabutaleb.dev@gmail.com>
2025-08-14 01:40:48 +0600

    8th done

--- script.js
+++ script.js
@@ -117,8 +117,20 @@
 // 40–59: "D"
 // 0–39: "F"
 
-let marks = -6;
+let marks = 90;
 
-if (marks < 0 && marks > 100) {
+if (marks > 100 || marks < 0) {
   console.log("Please enter valid number");
+} else if (marks >= 80 && marks <= 100) {
+  console.log("Your gread =  A+");
+} else if (marks >= 70 && marks <= 79) {
+  console.log("Your gread =  A");
+} else if (marks >= 60 && marks <= 69) {
+  console.log("Your gread =  B");
+} else if (marks >= 50 && marks <= 59) {
+  console.log("Your gread =  C");
+} else if (marks >= 33 && marks <= 49) {
+  console.log("Your gread =  D");
+} else if (marks >= 0 && marks <= 32) {
+  console.log("OH NO You are Fail");
 }
